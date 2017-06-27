56bd3d56a4cee40e2d1498816ae4d99500f679e0
Tim Murphy <tim.murphy86@gmail.com>
2017-06-26 20:42:51 -0400

    facebook login removing extra code

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var userID;
+
 
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
@@ -26,16 +28,16 @@ window.fbAsyncInit = function() {
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=816250271863348";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+// (function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=816250271863348";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
 
 FB.api('/me', {fields: 'id'}, function(response) {
-  console.log(response);
+  userID = response;
 });
 
 
