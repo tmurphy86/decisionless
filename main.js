00c90c6b00198fad5d870b003634143dad0b9a5f
Tim Murphy <tim.murphy86@gmail.com>
2017-06-26 20:48:55 -0400

    firebase config added

--- main.js
+++ main.js
@@ -40,6 +40,19 @@ FB.api('/me', {fields: 'id'}, function(response) {
   userID = response;
 });
 
+//FIREBASE CONFIG
+var config = {
+    apiKey: "AIzaSyBuEVQcL8R7uPoLiDrobj5HJVMfGI4UnX8",
+    authDomain: "murphytech-6f573.firebaseapp.com",
+    databaseURL: "https://murphytech-6f573.firebaseio.com",
+    projectId: "murphytech-6f573",
+    storageBucket: "murphytech-6f573.appspot.com",
+    messagingSenderId: "158805281949"
+  };
+  firebase.initializeApp(config);
 
+
+
+  console.log(userID);
 
 });
