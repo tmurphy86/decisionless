e9c6ca39b0fb2ea102b87739a3529df81252ecb0
Tim Murphy <tim.murphy86@gmail.com>
2017-06-28 20:56:28 -0400

    added logic for adding to db if not there

--- main.js
+++ main.js
@@ -1,116 +1,110 @@
 
-    //FACEBOOK AUTH SDK 
-    window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '816250271863348',
-          cookie     : true,
-          xfbml      : true,
-          version    : 'v2.8'
-        });
-        FB.AppEvents.logPageView();
-        $(document).trigger('fbload');
-      };
+//FACEBOOK AUTH SDK 
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '816250271863348',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v2.8'
+  });
+  FB.AppEvents.logPageView();
+  $(document).trigger('fbload');
+};
 
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "https://connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-
-
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
 
 
 
-
+//global variables
 var userID;
 
 
-
-
+//on document load function
 $(function(){
+
+
+//FIREBASE CONFIG
+var config = {
+    apiKey: "AIzaSyBuEVQcL8R7uPoLiDrobj5HJVMfGI4UnX8",
+    authDomain: "murphytech-6f573.firebaseapp.com",
+    databaseURL: "https://murphytech-6f573.firebaseio.com",
+    projectId: "murphytech-6f573",
+    storageBucket: "murphytech-6f573.appspot.com",
+    messagingSenderId: "158805281949"
+  };
+  firebase.initializeApp(config);
+
+  // Create a variable to reference the database.
+var database = firebase.database();
+// All of our connections will be stored in this directory.
+var fbDB = database.ref("/decisionless");
 
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
   statusChangeCallback(response);
   console.log(response, userID);
   });
-  }
-$(document).on(
-    'fbload',  //  <---- HERE'S OUR CUSTOM EVENT BEING LISTENED FOR
+}
+
+$(document).on('fbload',  //  <---- HERE'S OUR CUSTOM EVENT for FB load
+   
     function(){
-        //some code that requires the FB object
-        //such as...
         FB.getLoginStatus(function(res){
           console.log(res.status);
             if( res.status == "connected" ){
               console.log("working");
-                // FB.api('/me', function(fbUser) {
-                //     console.log("Open the pod bay doors, " + fbUser.name + ".");
-                // });
+                FB.api('/me', function(fbUser) {
+                    console.log(fbUser);
+                FB.api('/me', {fields: 'id'}, function(response) {
+                   userID = response;
+                   checkIfUserExists(userId) //coded after push on ready
+
+
+
+
+              console.log(userID);
+              });
+
+                });
             }
         });
 
     }
 );
-      
 
 
 
-      // checkLoginState(); still causing an undefined error
+function userExistsCallback(userId, exists) {
+  if (exists) {
+    alert('user ' + userId + ' exists!');
+  } else {
+                    database.ref("/decisionless").push({
+                    ID: userID,
+                    visted: userID//beenTo Array
+                 });
+    alert('user ' + userId + ' does not exist!');
+  }
+}
 
-//checking FB login status
-// console.log(checkLoginState);
-// if(checkLoginState) {
-
-//    FB.api('/me', {fields: 'id'}, function(response) {
-
-//       userID = response;
-
-//    database.ref("/decisionless").push({
-//         ID: userID,
-//         visted: userID//beenTo Array
-//       });
-
-//       console.log(userID);
-
-//   }); //.bind(this) pulled out
-// }
-
-
-$('.fb-login-button').on('click', function(){
-
-    FB.api('/me', {fields: 'id'}, function(response) {
-      userID = response;
-      // if (userID)
-
-   database.ref("/decisionless").push({
-        ID: userID,
-        visted: userID//beenTo Array
-      });
-
-      console.log(userID);
+// Tests to see if /users/<userId> has any data. 
+function checkIfUserExists(userId) {
+  fbDB.child(userId).once('value', function(snapshot) {
+    console.log("checking if user is there" + snapshot);
+    var exists = (snapshot.val() !== null);
+    userExistsCallback(userId, exists);
+  });
+}
 
 
-    });
-});
 
-    //FIREBASE CONFIG
-    var config = {
-        apiKey: "AIzaSyBuEVQcL8R7uPoLiDrobj5HJVMfGI4UnX8",
-        authDomain: "murphytech-6f573.firebaseapp.com",
-        databaseURL: "https://murphytech-6f573.firebaseio.com",
-        projectId: "murphytech-6f573",
-        storageBucket: "murphytech-6f573.appspot.com",
-        messagingSenderId: "158805281949"
-      };
-      firebase.initializeApp(config);
 
-      // Create a variable to reference the database.
-    var database = firebase.database();
-    // All of our connections will be stored in this directory.
-    var fbDB = database.ref("/decisionless");
+
 
 
 
