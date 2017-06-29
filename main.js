723f65016e79a608c947b245149fe6240bf14f4c
Tim Murphy <tim.murphy86@gmail.com>
2017-06-28 21:42:59 -0400

    checking error

--- main.js
+++ main.js
@@ -93,6 +93,8 @@ function userExistsCallback(userID, exists) {
 
 // Tests to see if /users/<userId> has any data. 
 function checkIfUserExists(userId) {
+  console.log("entering function exists" + userID, fbDB);
+
   fbDB.child(userID).once('value', function(snapshot) {
     console.log("checking if user is there" + snapshot);
     var exists = (snapshot.val() !== null);
