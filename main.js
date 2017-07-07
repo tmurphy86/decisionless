2810a80d03e51050e885bdd0b97d3b70c052e449
Tim Murphy <tim.murphy86@gmail.com>
2017-07-07 14:11:52 -0400

    google api for search places added

--- main.js
+++ main.js
@@ -151,6 +151,8 @@ $(document).on('fbload',  //  <---- HERE'S OUR CUSTOM EVENT for FB load
       zoom: 14
     });
     infoWindow = new google.maps.InfoWindow;
+    service = new google.maps.places.PlacesService(map);
+    map.addListener('idle', performSearch);
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -169,6 +171,47 @@ $(document).on('fbload',  //  <---- HERE'S OUR CUSTOM EVENT for FB load
       handleLocationError(false, infoWindow, map.getCenter());
     }
   }
+
+  function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    keyword: 'best view'
+  };
+  service.radarSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status !== google.maps.places.PlacesServiceStatus.OK) {
+    console.error(status);
+    return;
+  }
+  for (var i = 0, result; result = results[i]; i++) {
+    addMarker(result);
+  }
+}
+
+function addMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: {
+      url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png',
+      anchor: new google.maps.Point(10, 10),
+      scaledSize: new google.maps.Size(10, 17)
+    }
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    service.getDetails(place, function(result, status) {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        console.error(status);
+        return;
+      }
+      infoWindow.setContent(result.name);
+      infoWindow.open(map, marker);
+    });
+  });
+}
 
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
